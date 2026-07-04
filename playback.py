fd5b4141558b0da024ccc9cd0700b1223cb1f275
Shoaibcraft <Shoaibcraft@users.noreply.github.com>
2026-07-04 18:24:01 -0500

    automated commit by check50 [check50=True]

---
+++ playback.py
@@ -0,0 +1,8 @@
+# Prompt the user for input
+user_input = input("Please enter some text: ")
+
+# Replace every space with three periods
+output = user_input.replace(" ", "...")
+
+# Print the result
+print(output)
