4794964e74ad911068ab5bd51eabe00ae00d22fc
Shoaib Ahmad <shoaib.a.edu@gmail.com>
2026-07-05 01:07:06 -0500

    improve comments in einstein.py
    Fixed typos in comments and formatting.

--- einstein.py
+++ einstein.py
@@ -1,10 +1,14 @@
+# Definne a funcrion where the Energy formula will be applied
 def formula(mass, speed_of_light):
   E = mass*speed_of_light*speed_of_light
+  # Get the answer back as return value
   return E
 
 def main():
+  # Take mass input
   m = int(input("Please enter the value of mass in kg: "))
   c = 300000000
+  # Take output using input formula
   print("The answer is", formula(m, c))
 
 main()
