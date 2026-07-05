59fc9b90f59ff6ce761329c696aa422dcb33b042
Shoaibcraft <Shoaibcraft@users.noreply.github.com>
2026-07-04 19:04:54 -0500

    automated commit by check50 [check50=True]

---
+++ faces.py
@@ -0,0 +1,15 @@
+def convert(text):
+  # Replace emoticons with emoji
+  # Note: Use the actual emoji characters here
+  text = text.replace(":)", "😁")
+  text = text.replace(":(", "😒")
+  return text
+
+def main():
+  # Take input from user
+  user_input = input("")
+  # output and calling the convert function
+  print(convert(user_input))
+
+# Call main function for execution
+main()
